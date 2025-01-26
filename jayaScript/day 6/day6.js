685961927400c5320df52f30ca9392e2d4a7b5bc
Harshda95 <harshdagargi@gmail.com>
2025-01-26 16:10:06 +0530

    making password validator

--- jayaScript/day 6/day6.js
+++ jayaScript/day 6/day6.js
@@ -1,21 +1,38 @@
-console.log("passwoed")
-let password = prompt("enter the password")
-if(password.length<8){
-    alert("please at least 8 characters")
-   
-}
+
+
+let user = document.getElementById("password")
+
+document.getElementById("btn").addEventListener( "click", () =>{
+    let password = user.value;
+    let msg = document.getElementById("output")
+    let submitBtn=document.getElementById("btn")
+ if (password.length < 8) {
+    msg.textContent="password must have 8 characters"
+    msg.style.color="red"
+   submitBtn.style.backgroundColor="red"
+    
+ }
 
  else if (!/[A-Z]/.test(password)) {
-    alert("please enter at least a uppper case")
-  
+    msg.textContent="password have at least one upppercase Character"
+    msg.style.color="red"
+    submitBtn.style.backgroundColor="red"
     
-} 
- else if( !/[0-9]/.test(password)){
-    alert("please enter at least a  digit")
-   
+ } 
+ else if(!/[0-9]/.test(password)) {
+    msg.textContent="password must have atleast ome no."
+    msg.style.color="red"
+    submitBtn.style.backgroundColor="red"
+    
+ }
+ 
+ 
+ else {
+    msg.textContent="password is valid "
+    msg.style.color="Green"
+    submitBtn.style.backgroundColor="green"
+    alert("password is submitted") 
+ }
 
-}
-else {
-    alert("your password submitted sucessfully")
-    
-}
+
+})
